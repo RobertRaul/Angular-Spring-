8ec6dfb6fef325f91757be83e36341abca90f86a
Robert Raul <lordnebr4@gmail.com>
2024-02-19 11:46:16 -0500

    Completado el curso

--- backend/src/main/java/gm/inventarios/controlador/ProductoControlador.java
+++ backend/src/main/java/gm/inventarios/controlador/ProductoControlador.java
@@ -1,20 +1,21 @@
 package gm.inventarios.controlador;
 
+import gm.inventarios.controlador.excepciones.NoEncontrado;
 import gm.inventarios.modelo.Producto;
 import gm.inventarios.servicio.ProductoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("inventario-app")
-@CrossOrigin(value="http:localhost:4200")
+@RequestMapping("app")
+@CrossOrigin(value="http://localhost:4200")
 public class ProductoControlador {
 
     Logger logger = LoggerFactory.getLogger(ProductoControlador.class);
@@ -28,5 +29,44 @@ public class ProductoControlador {
         logger.info("Productos Obtenidos");
         productos.forEach((producto -> logger.info(producto.toString())));
         return productos;
+    }
+
+    @PostMapping("/productos")
+    public Producto aregarProducto(@RequestBody Producto producto){
+        logger.info("Productos Registrado: "+ producto);
+        return this.productoServicio.guardarProducto(producto);
+    }
+
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> ObtenerProductoPorId(@PathVariable int id){
+        Producto producto = this.productoServicio.buscarProductoPorId(id);
+        if(producto !=null)
+            return  ResponseEntity.ok(producto);
+        else
+            throw new NoEncontrado("No se encontro ese id"+ id);
+    }
+
+    @PutMapping("/productos/{id}")
+    public ResponseEntity<Producto> actualizarProducto(@PathVariable int id,@RequestBody Producto productoRecibido){
+        Producto producto = this.productoServicio.buscarProductoPorId(id);
+        if(producto == null)
+            throw  new NoEncontrado("Producto no encontradoID:"+id);
+        producto.setDescripcion(productoRecibido.getDescripcion());
+        producto.setPrecio(productoRecibido.getPrecio());
+        producto.setExistencia(productoRecibido.getExistencia());
+        this.productoServicio.guardarProducto(producto);
+        return  ResponseEntity.ok(producto);
+    }
+
+
+    @DeleteMapping("/productos/{id}")
+    public ResponseEntity<Map<String,Boolean>> eliminarProducto(@PathVariable int id){
+        Producto producto = productoServicio.buscarProductoPorId(id);
+        if(producto == null)
+            throw  new NoEncontrado("Producto no encontradoID:"+id);
+        this.productoServicio.eliminarProductoPorId(producto.getIdProducto());
+        Map<String,Boolean>respues = new HashMap<>();
+        respues.put("eliminado",Boolean.TRUE);
+        return ResponseEntity.ok(respues);
     }
 }
--- backend/src/main/java/gm/inventarios/modelo/Producto.java
+++ backend/src/main/java/gm/inventarios/modelo/Producto.java
@@ -4,14 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Configurable;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +16,37 @@ public class Producto {
     String descripcion;
     Double precio;
     Integer existencia;
+
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(Integer existencia) {
+        this.existencia = existencia;
+    }
 }
